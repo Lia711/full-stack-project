dde9a251702393f520b50924639d88ad09108a23
Liana Petrova <lianapetrova00@gmail.com>
2023-06-02 11:33:45 +0100

    Data can now be fetched by front end

---
+++ project-zelda-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import {React, useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+
+  const [games, setGames] = useState([]);
+
+  const getAllGames = async()=> {
+    // {headers: {'Access-Control-Allow-Origin':'*'}}
+    let url=`http://localhost:8080/games`;
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setGames(data)
+    } catch (e) {
+      console.log("this is the error", e)
+    }
+  }
+  console.log(games);
+
+  useEffect(()=>{
+    getAllGames();
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Zelda Games</h1>
+      <div>{games}</div>
+    </div>
+  );
+}
+
+export default App;
--- zelda/src/main/java/com/example/zelda/AppController.java
+++ zelda/src/main/java/com/example/zelda/AppController.java
@@ -1,5 +1,6 @@
 package com.example.zelda;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AppController {
     List<String> games = new ArrayList<>();
     {
